2989d880b7366ab60e6ae8a07324800bb8482016
hilluri <hilluri@asu.edu>
2018-04-25 18:37:20 -0700

    first push

--- mcfinal/views.py
+++ mcfinal/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
-
+import time
 from django.http import HttpResponse, HttpRequest
 def test(request):
+    time.sleep(10)
     return HttpResponse('hello')
