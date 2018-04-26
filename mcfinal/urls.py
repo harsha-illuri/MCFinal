998ffe07c65ce73d47133aeef6238e459e3e8c24
hilluri <hilluri@asu.edu>
2018-04-25 17:47:52 -0700

    first push

---
+++ mcfinal/urls.py
@@ -0,0 +1,9 @@
+from django import urls
+
+from django.contrib import admin
+from django.urls import path, include
+
+from mcfinal import views
+urlpatterns = [
+    path('$/', views.test, name = 'test'),
+]
